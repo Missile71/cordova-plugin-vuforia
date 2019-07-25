15e157d1c8788e677bfacab520771bd718f2667b
Stefano <13023029+Missile71@users.noreply.github.com>
2019-07-25 20:06:02 +0200

    Update ViewController.mm

--- src/ios/ViewController.mm
+++ src/ios/ViewController.mm
@@ -13,6 +13,20 @@
 
 @implementation ViewController
 
+-(void) showHTML:(NSString*)url markerid:(NSString *)markerid{
+    if (markerid != self.imageTargetsViewController.markerid){
+        self.imageTargetsViewController.book_id = self.book_id;
+        self.imageTargetsViewController.email = self.email;
+        self.imageTargetsViewController.prof_fullname = self.prof_fullname;
+        self.imageTargetsViewController.prof_id = self.prof_id;
+        self.imageTargetsViewController.extra = self.extra;
+        self.imageTargetsViewController.path_documents = self.path_documents;
+        self.imageTargetsViewController.tipo = self.tipo;
+        
+        [self.imageTargetsViewController showHTML:url markerid:markerid];
+    }
+    
+}
 
 -(id)initWithFileName:(NSString *)fileName targetNames:(NSArray *)imageTargetNames overlayOptions:( NSDictionary *)overlayOptions vuforiaLicenseKey:(NSString *)vuforiaLicenseKey {
 
@@ -24,6 +38,56 @@
     NSLog(@"Vuforia Plugin :: overlayOptions: %@", self.overlayOptions);
     NSLog(@"Vuforia Plugin :: License key: %@", self.vuforiaLicenseKey);
     return self;
+}
+
+-(id)initWithFileNameZanichelli:(NSString *)fileName targetNames:(NSArray *)imageTargetNames overlayOptions:( NSDictionary *)overlayOptions vuforiaLicenseKey:(NSString *)vuforiaLicenseKey book_id:(NSString *)book_id email:(NSString *)email prof_fullname:(NSString *)profFullName prof_id:(NSString *)profId extra:(NSString *)extra pathDocuments:(NSString *)path_documents tipo:(NSString *)tipo fileName2:(NSString *)fileName2{
+    
+    self = [super init];
+    self.imageTargets = [[NSDictionary alloc] initWithObjectsAndKeys: fileName, @"imageTargetFile", fileName2, @"imageTargetFile2", imageTargetNames, @"imageTargetNames", nil];
+    self.overlayOptions = overlayOptions;
+    self.vuforiaLicenseKey = vuforiaLicenseKey;
+    NSLog(@"Vuforia Plugin :: initWithFileName: %@", fileName);
+    NSLog(@"Vuforia Plugin :: overlayOptions: %@", self.overlayOptions);
+    NSLog(@"Vuforia Plugin :: License key: %@", self.vuforiaLicenseKey);
+    
+    //MI SALVO I PARAMETRI RICEVUTI
+    self.book_id = book_id;
+    self.email = email;
+    self.prof_fullname = profFullName;
+    self.prof_id = profId;
+    self.extra = extra;
+    self.path_documents = path_documents;
+    self.tipo = tipo;
+    
+    return self;
+}
+    
+-(id)initWithFileNameZanichelli_OLD:(NSString *)fileName targetNames:(NSArray *)imageTargetNames overlayOptions:( NSDictionary *)overlayOptions vuforiaLicenseKey:(NSString *)vuforiaLicenseKey book_id:(NSString *)book_id email:(NSString *)email prof_fullname:(NSString *)profFullName prof_id:(NSString *)profId extra:(NSString *)extra pathDocuments:(NSString *)path_documents tipo:(NSString *)tipo {
+    
+    self = [super init];
+    self.imageTargets = [[NSDictionary alloc] initWithObjectsAndKeys: fileName, @"imageTargetFile", imageTargetNames, @"imageTargetNames", nil];
+    self.overlayOptions = overlayOptions;
+    self.vuforiaLicenseKey = vuforiaLicenseKey;
+    NSLog(@"Vuforia Plugin :: initWithFileName: %@", fileName);
+    NSLog(@"Vuforia Plugin :: overlayOptions: %@", self.overlayOptions);
+    NSLog(@"Vuforia Plugin :: License key: %@", self.vuforiaLicenseKey);
+    
+    //MI SALVO I PARAMETRI RICEVUTI
+    self.book_id = book_id;
+    self.email = email;
+    self.prof_fullname = profFullName;
+    self.prof_id = profId;
+    self.extra = extra;
+    self.path_documents = path_documents;
+    self.tipo = tipo;
+    
+    return self;
+}
+
+- (void) webViewDidFinishLoad:(UIWebView *)webView
+{
+    //Execute javascript method or pure javascript if needed
+    //[_webView stringByEvaluatingJavaScriptFromString:@"methodName();"];
 }
 
 - (void)viewDidLoad {
@@ -43,9 +107,11 @@
         self.launchedCamera = true;
 
         self.imageTargetsViewController.imageTargetFile = [self.imageTargets objectForKey:@"imageTargetFile"];
+        self.imageTargetsViewController.imageTargetFile2 = [self.imageTargets objectForKey:@"imageTargetFile2"];
         self.imageTargetsViewController.imageTargetNames = [self.imageTargets objectForKey:@"imageTargetNames"];
 
         [self presentViewController:self.imageTargetsViewController animated:NO completion:nil];
+        self.imageTargetsViewController.vc = self;
     }
 }
 
@@ -87,5 +153,7 @@
 {
     return [[self presentingViewController] preferredInterfaceOrientationForPresentation];
 }
+
+
 
 @end
