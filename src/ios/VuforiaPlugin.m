c6ecd1fb96ef0b88dcd466941008d5b8de617cd9
Stefano <13023029+Missile71@users.noreply.github.com>
2019-07-25 20:04:55 +0200

    Update VuforiaPlugin.m

--- src/ios/VuforiaPlugin.m
+++ src/ios/VuforiaPlugin.m
@@ -1,5 +1,6 @@
 #import "VuforiaPlugin.h"
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface VuforiaPlugin()
 
@@ -7,15 +8,49 @@
 @property ViewController *imageRecViewController;
 @property BOOL startedVuforia;
 @property BOOL autostopOnImageFound;
+    
 
 @end
 
 @implementation VuforiaPlugin
+    
+- (void) cordovaAddHTML:(CDVInvokedUrlCommand *)command {
+    NSLog(@"Vuforia Plugin :: cordovaAddHTML");
+    [self.imageRecViewController showHTML:[command.arguments objectAtIndex:0] markerid:[command.arguments objectAtIndex:1]];
+}
 
 - (void) cordovaStartVuforia:(CDVInvokedUrlCommand *)command {
 
     NSLog(@"Vuforia Plugin :: Start plugin");
 
+    NSString *mediaType = AVMediaTypeVideo;
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:mediaType];
+    if(authStatus == AVAuthorizationStatusAuthorized) {
+        // do your logic
+    } else if(authStatus == AVAuthorizationStatusDenied){
+        // denied
+        NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleNameKey];
+        NSString *message = [NSString stringWithFormat:@"Per dare l'autorizzazione vai su: \nSettings > Privacy > Camera > %@ e seleziona ON.", appName];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Non è stato consentito l'uso della fotocamera!" message:message delegate:self cancelButtonTitle:@"Chiudi" otherButtonTitles:nil, nil];
+        
+        [alert show];
+        return;
+    } else if(authStatus == AVAuthorizationStatusRestricted){
+        // restricted, normally won't happen
+    } else if(authStatus == AVAuthorizationStatusNotDetermined){
+        // not determined?!
+        NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleNameKey];
+        NSString *message = [NSString stringWithFormat:@"User denied camera access to this App. To restore camera access, go to: \nSettings > Privacy > Camera > %@ and turn it ON.", appName];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"iOS8 Camera Access Warning" message:message delegate:self cancelButtonTitle:@"Close" otherButtonTitles:nil, nil];
+        
+        [alert show];
+        return;
+    } else {
+        // impossible, unknown authorization status
+    }
+    
     NSLog(@"Arguments: %@", command.arguments);
     NSLog(@"KEY: %@", [command.arguments objectAtIndex:3]);
 
@@ -25,7 +60,7 @@
 
     self.autostopOnImageFound = [[command.arguments objectAtIndex:6] integerValue];
 
-    [self startVuforiaWithImageTargetFile:[command.arguments objectAtIndex:0] imageTargetNames: [command.arguments objectAtIndex:1] overlayOptions: overlayOptions vuforiaLicenseKey: [command.arguments objectAtIndex:3]];
+    [self startVuforiaWithImageTargetFileZanichelli:[command.arguments objectAtIndex:0] imageTargetNames: [command.arguments objectAtIndex:1] overlayOptions: overlayOptions vuforiaLicenseKey: [command.arguments objectAtIndex:3] book_id:[command.arguments objectAtIndex:7] email:[command.arguments objectAtIndex:8] prof_fullname:[command.arguments objectAtIndex:9] prof_id:[command.arguments objectAtIndex:10] extra:@"" pathDocuments:[command.arguments objectAtIndex:11] tipo:[command.arguments objectAtIndex:12] imageTargetfile2:[command.arguments objectAtIndex:10]];
     self.command = command;
 
     self.startedVuforia = true;
@@ -108,12 +143,26 @@
 
     self.imageRecViewController = [[ViewController alloc] initWithFileName:imageTargetfile targetNames:imageTargetNames overlayOptions:overlayOptions vuforiaLicenseKey:vuforiaLicenseKey];
 
+    
+    [self.viewController presentViewController:self.imageRecViewController animated:YES completion:nil];
+}
+
+- (void) startVuforiaWithImageTargetFileZanichelli:(NSString *)imageTargetfile imageTargetNames:(NSArray *)imageTargetNames overlayOptions:(NSDictionary *)overlayOptions vuforiaLicenseKey:(NSString *)vuforiaLicenseKey book_id:(NSString *)book_id email:(NSString *)email prof_fullname:(NSString *)profFullName prof_id:(NSString *)profId extra:(NSString *)extra pathDocuments:(NSString *)path_documents tipo:(NSString *)tipo imageTargetfile2:(NSString *)imageTargetfile2{
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"ImageMatched" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(imageMatched:) name:@"ImageMatched" object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"CloseRequest" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(closeRequest:) name:@"CloseRequest" object:nil];
+    
+//    self.imageRecViewController = [[ViewController alloc] initWithFileName:imageTargetfile targetNames:imageTargetNames overlayOptions:overlayOptions vuforiaLicenseKey:vuforiaLicenseKey];
+    
+    self.imageRecViewController = [[ViewController alloc] initWithFileNameZanichelli:imageTargetfile targetNames:imageTargetNames overlayOptions:overlayOptions vuforiaLicenseKey:vuforiaLicenseKey book_id:book_id email:email prof_fullname:profFullName prof_id:profId extra:extra pathDocuments:path_documents tipo:tipo fileName2:imageTargetfile2];
+    
     [self.viewController presentViewController:self.imageRecViewController animated:YES completion:nil];
 }
 
 
 - (void)imageMatched:(NSNotification *)notification {
-
     NSDictionary* userInfo = notification.userInfo;
 
     NSLog(@"Vuforia Plugin :: image matched");
@@ -129,6 +178,9 @@
 
     if(self.autostopOnImageFound){
         [self VP_closeView];
+    } else {
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:@"ImageMatched" object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(imageMatched:) name:@"ImageMatched" object:nil];
     }
 }
 
@@ -136,7 +188,7 @@
 
     NSDictionary* jsonObj = @{@"status": @{@"manuallyClosed": @true, @"message": @"User manually closed the plugin."}};
 
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsDictionary: jsonObj];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_NO_RESULT messageAsDictionary: jsonObj];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.command.callbackId];
     [self VP_closeView];
